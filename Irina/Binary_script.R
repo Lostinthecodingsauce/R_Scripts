4b29830baf670454fc661dd36000e19a5d933e0d
Zquinlan <zquinlan@gmail.com>
2019-01-31 15:47:32 -0800

    Made file for Irina

---
+++ Irina/Binary_script.R
@@ -0,0 +1,20 @@
+canopus <- read_csv("canopus_summary.tsv.csv")
+
+canopus_binary <- canopus[2:ncol(canopus)]
+canopus_binary[canopus_binary > 0.75] <- 1
+canopus_binary[canopus_binary < 0.75] <- 0
+canopus_binary <- cbind(canopus$name, canopus_binary)
+
+
+write_csv(canopus_binary, "test.csv")
+
+
+matric <- matrix(norm(5*6, mean = 20, sd = 2),5,6)
+'>5'
+
+
+Irina_yes <- Irina_fixed%>%
+  dplyr::select(-c(newcolumn))
+
+Irina_important <- Irina_yes%>%
+  dplyr::filter()
