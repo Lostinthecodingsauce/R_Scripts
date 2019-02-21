1781b303954e282c02c0d864e696ad197bf9f0cc
Zquinlan <zquinlan@gmail.com>
2019-02-21 13:08:19 -0800

    subsetting dorcierr

--- pipelines/metabalomics/moorea2017/Moorea2017Metabalomics.R
+++ pipelines/metabalomics/moorea2017/Moorea2017Metabalomics.R
@@ -12,6 +12,7 @@ library(DescTools)
 library(vegan)
 library(ape)
 library(wesanderson)
+library(RColorBrewer)
 
 # Loading in Data and Joining Tables ---------------------------------------------------------
 
@@ -36,21 +37,18 @@ scan_IDs <- as.data.frame(old_hits$Scan_Number)
 
 # Transposing and selecting only sample data
 samples <- both_runs_by_old%>%
-  select(c(Feature_Name:D_DT_2_T0N_C18))%>%
-  gather(sample_ID, RA, 2:318)%>%
+  select(c('Feature_Name', '% of Total(D_WA_1_T0D)':'% of Total(R_TR_3_TFN)'))%>%
+  gather(sample_ID, RA, 2:278)%>%
   spread(Feature_Name, RA)
 
 # Some Samples had - instead of _
 samples$sample_ID <- gsub("-", "_", samples$sample_ID)
+samples$sample_ID <- gsub("% of Total\\(", "", samples$sample_ID)
+samples$sample_ID <- gsub(")", "", samples$sample_ID)
 
 # renaming columns and row metadata
 moorea_wdf <-samples%>%
-  separate(sample_ID, paste("c", 1:5, sep = "."), sep = "_")%>%
-  rename(Experiment = `c.1`,
-         Organism = `c.2`,
-         Replicate = `c.3`,
-         Timepoint = `c.4`,
-         DOM_source = `c.5`)%>%
+  separate(sample_ID, c("Experiment", "Organism", "Replicate", "Timepoint", "DOM_source"), sep = "_")%>%
   filter(!Experiment %like% "Blank")%>%
   filter(!Experiment == "SE",
          !Experiment == "SR")%>%
@@ -78,34 +76,33 @@ moorea_wdf <-samples%>%
                                      Organism == "WA" ~ "Water control",
                                      TRUE ~ as.character(Organism)))
 
-moorea_wdf$Organismal_clade <- moorea_wdf$Organism
+moorea_wdf <- apply(moorea_wdf[6:10031], 2, function(x) gsub("%", "", x))
+# moorea_wdf$Organismal_clade <- moorea_wdf$Organism
+# 
+# moorea_wdf <- moorea_wdf%>%
+#   dplyr::mutate(Organismal_clade = case_when(Organismal_clade == "Outside control" ~ "water",
+#                                              Organismal_clade == "Tent control" ~ "water",
+#                                              Organismal_clade == "Water control" ~ "water",
+#                                              Organismal_clade == "Porites lobata" ~ "coral",
+#                                              Organismal_clade == "Pocillopora verrucosa" ~ "coral",
+#                                              Organismal_clade %like any% c(1:20) ~ "environmental",
+#                                              Organismal_clade == "Dictyota" ~ "algae",
+#                                              Organismal_clade == "Turf" ~ "algae",
+#                                              Organismal_clade == "Coral_control" ~ "coral",
+#                                              Organismal_clade == "Coral_labile" ~ "coral",
+#                                              Organismal_clade == "Coral_recalcitrant" ~ "coral",
+#                                              Organismal_clade == "Algae_control" ~ "algae + coral",
+#                                              Organismal_clade == "Algae_labile" ~ "algae + coral",
+#                                              Organismal_clade == "Algae_recalcitrant" ~ "algae + coral",
+#                                              Organismal_clade == "Algae" ~ "algae",
+#                                              TRUE ~ as.character(Organismal_clade)))%>%
+#   dplyr::filter(!Organismal_clade == "Blank")
+#   
+# moorea_wdf["Timepoint"][is.na(moorea_wdf["Timepoint"])] <- 0
 
-moorea_wdf <- moorea_wdf%>%
-  dplyr::mutate(Organismal_clade = case_when(Organismal_clade == "Outside control" ~ "water",
-                                             Organismal_clade == "Tent control" ~ "water",
-                                             Organismal_clade == "Water control" ~ "water",
-                                             Organismal_clade == "Porites lobata" ~ "coral",
-                                             Organismal_clade == "Pocillopora verrucosa" ~ "coral",
-                                             Organismal_clade %like any% c(1:20) ~ "environmental",
-                                             Organismal_clade == "Dictyota" ~ "algae",
-                                             Organismal_clade == "Turf" ~ "algae",
-                                             Organismal_clade == "Coral_control" ~ "coral",
-                                             Organismal_clade == "Coral_labile" ~ "coral",
-                                             Organismal_clade == "Coral_recalcitrant" ~ "coral",
-                                             Organismal_clade == "Algae_control" ~ "algae + coral",
-                                             Organismal_clade == "Algae_labile" ~ "algae + coral",
-                                             Organismal_clade == "Algae_recalcitrant" ~ "algae + coral",
-                                             Organismal_clade == "Algae" ~ "algae",
-                                             TRUE ~ as.character(Organismal_clade)))%>%
-  dplyr::filter(!Organismal_clade == "Blank")
-  
-moorea_wdf["Timepoint"][is.na(moorea_wdf["Timepoint"])] <- 0
-
-moorea_wdf_fix <- moorea_wdf%>%
-  filter(!Timepoint == "T0D",
-         !Timepoint == "T0N")
-
-
+dorcierr <- moorea_wdf%>%
+  filter(Experiment == "dorcierr")%>%
+  select(-"DOM_source")
 
 mordor <- moorea_wdf%>%
   filter(Experiment == "mordor")
@@ -114,12 +111,23 @@ big3 <- moorea_wdf%>%
   filter(!Experiment == "LoRDI",
          !Experiment == "SPIFFy")
 
+# dorcierr further cleaning -----------------------------------------------
+dorcierr_wdf <- dorcierr%>%
+  separate(Timepoint, c("Timepoint", "DayNight"), sep = -1)%>%
+  mutate(DayNight = case_when(DayNight == "D" ~ "Day",
+                              TRUE ~ "Night"))
+
+dorcierr_final <- dorcierr_wdf%>%
+  filter(Timepoint == "TF")
+
+dorcierr_transformations <- dorcierr_wdf%>%
+  gather()
 
 # PCoA --------------------------------------------------------------------
 
 #making abundance only matrix and saving columns with names/metadata into dinames
-microb_f <- moorea_wdf_fix%>%
-  select(-c(1:5, Organismal_clade))
+microb_f <- dorcierr_final%>%
+  select(-c(1:5))
 
 veg_bray <- vegdist(microb_f, "bray") #Bray-curtis distances
 
@@ -136,16 +144,17 @@ text(x = Eigan, y = pc_scores$values$Relative_eig[1:10], label = pc_scores$value
 # Plot using ggplot2
 pco_scores <- as.data.frame(pc_scores$vectors)
 
-ggplot(pco_scores, mapping = aes(Axis.1, Axis.2, col = moorea_wdf_fix$Organismal_clade, 
-                                 shape = factor(moorea_wdf_fix$Experiment))) +
-  geom_point(stat = "identity") +
-  scale_shape_manual(values = c(0,1,2,3,5,7,19,9))
+ggplot(pco_scores, mapping = aes(Axis.1, Axis.2, col = dorcierr_final$Organism, 
+                                 shape = factor(dorcierr_final$DayNight))) +
+  geom_point(stat = "identity")+
+  scale_shape_manual(values = c(1,19)) +
+  scale_color_brewer(type = "qual", palette = 2)
 
 # PERMANOVA ---------------------------------------------------------------
 
 adonis(microb_f ~ Organismal_clade, moorea_wdf_fix, perm=1000, method="bray", set.seed(100))
 
-pairwiseAdonis::pairwise.adonis(microb_f, moorea_wdf_fix$Organismal_clade, p.adjust.m = "BH")
+pairwiseAdonis::pairwise.adonis(microb_f, dorcierr_wdf$Organism, p.adjust.m = "BH")
 # Writing CSVs ------------------------------------------------------------
 
 write_csv(moorea_wdf, "moorea_metab_FeatureID_samples.csv")
@@ -155,4 +164,5 @@ write_delim(scan_IDs, "Feature_ID.txt", delim = " ", col_names = FALSE)
 
 write_csv(both_runs_by_old, "new_by_old_AllMeta.csv")
 
+write_csv(dorcierr, "Dorcierr_metab_wdf.csv")
 
