7ed342bbc67628b5d2bb7da3cfbcdfe3d6ae89e1
Zquinlan <zquinlan@gmail.com>
2019-03-20 10:36:39 -0700

    fixing errors

--- pipelines/metabalomics/moorea2017/Moorea2017MetabalomicsBlankRemoval.R
+++ pipelines/metabalomics/moorea2017/Moorea2017MetabalomicsBlankRemoval.R
@@ -8,6 +8,7 @@ library(data.table)
 library(DescTools)
 library(broom)
 library(readxl)
+library(multcomp)
 
 #PCoA, PERMANOVA
 library(vegan)
@@ -36,11 +37,14 @@ canopus_anotations <- read_csv("SIRIUS_etc/converted/Canopus_classes.csv")
 # Sirius and Zodiac both try to assign molecular formulas to all the features
 sirius_zodiac_anotations <- read_csv("SIRIUS_etc/converted/SIRIUS_Zodiac_converted.csv")%>%
   rename(feature_number = 1)%>%
-  select(-c(14:ncol(.)))
+  dplyr::select(-c(14:ncol(.)))
 
 # Feature table has all features found within the experiments and blanks
 # The columns need to be changed to the actual experiment sample codes
-feature_table_raw <- read_csv("Morrea_Feayures-Table_all_Gap-Filled5.csv")
+# Feature_table_raw is the raw export from MZMine
+feature_table_raw <- read_csv("Morrea_Feayures-Table_all_Gap-Filled5.csv")%>%
+  rename('feature_number' = 'row ID')
+
 ms_sample_codes <- read_csv("Mo'orea 2017 Mass spec sample codes - Sheet1.csv")%>%
   rename('run_code' = 'Sample ID',
          'sample_code' = 'Sample Name')
@@ -59,19 +63,21 @@ super_computer_annotations <- full_join(canopus_best_match, sirius_zodiac_anotat
 ## join library hits, analog hits and super computer predictions
 metadata <- full_join(node_info, 
                       full_join(super_computer_annotations,
-                                full_join(true_hits, analog_hits, by = "feature_number", suffix = c("Library_", "Analog_")), 
-                                by = "feature_number"), 
+                                full_join(feature_table_raw[1:4],
+                                          full_join(true_hits, analog_hits, by = "feature_number",
+                                                    suffix = c("Library_", "Analog_")),
+                                          by = "feature_number"),
+                                by = "feature_number"),
                       by = "feature_number")
 
 metadata$feature_number <- as.character(metadata$feature_number)
 
 ## making feature table so we can remove blanks
 # have to change the MS codes for sample codes
-# selecting for Dorcierr, Mordor, RR3, Spiffy
+# dplyr::selecting for Dorcierr, Mordor, RR3, Spiffy
 feature_table_temp <- feature_table_raw%>%
-  select(-X326)%>%
-  select(-c(2:4))%>%
-  rename('feature_number' = 'row ID')%>%
+  dplyr::select(-X326)%>%
+  dplyr::select(-c(2:4))%>%
   gather(run_code, ion_charge, 2:ncol(.))%>%
   spread(feature_number, ion_charge)
 
@@ -80,7 +86,7 @@ feature_table_temp$run_code <- feature_table_temp$run_code%>%
   gsub("_MSMS", "", .)
 
 feature_table_dirty <- left_join(ms_sample_codes, feature_table_temp, by = "run_code")%>%
-  select(-run_code)%>%
+  dplyr::select(-run_code)%>%
   filter(!sample_code %like any% c("%_XAD", "%_C18", "SR%", "LoRDI%", "SE%"))%>%
   gather(feature_number, ion_charge, 2:20743)%>%
   spread(sample_code, ion_charge)
@@ -101,64 +107,74 @@ ions$max_blanks <- apply(ions[ions_blanks], 1, max)
 ions$mean_samples <- apply(ions[ions_samples], 1, mean)
 
 
-#This section finds the features where mean area under the curve across all samples is larger than .5 * max of the blanks
-#The non background features are saved into a vector so that they can be filtered from the master database
+# This section finds the features where mean area under the curve across all samples is larger than .5 * max of the blanks
+# The non background features are saved into a vector so that they can be filtered from the master database
+# mean(samples)*0.5 > max_blanks
 no_background <-ions%>%
-  filter(mean_samples, mean_samples > 2*max_blanks)
+  filter(mean_samples, mean_samples*0.5 > max_blanks)
 
 feature_table_no_background <- left_join(no_background[1], feature_table_dirty, by = "feature_number")
 
 ## Removing transient features ---------------------------------------------
 # Transient features are defined as features who's area under the curve is not more than 2E5 in at least 3 samples
-# This was determined by comparing gap filled and non-gap filled data and selecting for the lowest peak area in non-gap filled data
+# This was determined by comparing gap filled and non-gap filled data and dplyr::selecting for the lowest peak area in non-gap filled data
 # This gives the assumption that anything lower than 2E5 is noise. See supplemental figure
 feature_table_no_background <- cbind(feature_table_no_background,
                                   trans_feature_finder = 
-                                    rowSums(feature_table_no_background[ions_samples] > 2E5))%>%
+                                    rowSums(feature_table_no_background[ions_samples] > 2E5))
+
+feature_table_no_back_trans <- feature_table_no_background%>%
   filter(trans_feature_finder, trans_feature_finder >= 3)%>%
-  select(-trans_feature_finder)
+  dplyr::select(-trans_feature_finder)
 
 ## Calculate Total Ion Charge (TIC) and relativize samples -----------------
 ## FIND WHERE SCAN NUMBER IS GOING SO THAT THESE CAN ALL BE COMIBINED AGAIN
-feature_table_no_background$TIC <- apply(feature_table_no_background[ions_samples], 1, sum)
+feature_table_RA_temp <- feature_table_no_back_trans%>%
+  gather(sample_name, peak_area, 2:260)%>%
+  spread(feature_number, peak_area)%>%
+  add_column(TIC = apply(.[2:13539], 1, sum))
 
-# IonCharge/TIC = RA values for every feature/sample
+# IonCharge/TIC = RA values for every feature peak area /sum(sample peak areas)
 feature_relative_abundance <- 
   as.data.frame(
     sapply(
-      feature_table_no_background[ions_samples],
-      function(x) x/feature_table_no_background$TIC))%>%
-  add_column(feature_number = feature_table_no_background$feature_number)
+      feature_table_RA_temp[2:13539],
+      function(x) x/feature_table_RA_temp$TIC))%>%
+  add_column(sample_name = feature_table_RA_temp$sample_name, .before = 1)%>%
+  gather(feature_number, RA, 2:13539)%>%
+  spread(sample_name, RA)
 
 # angular transformation of RA
 feature_asin_sqrt <-
   as.data.frame(
     sapply(
-      feature_relative_abundance[1:250],
+      feature_relative_abundance[2:260],
       function(x) asin(sqrt(x))))%>%
-  add_column(feature_number = feature_table_no_background$feature_number)
+  add_column(feature_number = feature_table_no_back_trans$feature_number)
 
 # All three joined together (Peak area, RA, asin(sqrt))
-# Node and network info = [1:22], CANOPUS = [23:25], SIRIUS/ZODIAC = [26:38], Library Hits = [39:65], analog hits = []
-# Blanks = [109:116], Area under the curve = [117:367], RA = [369:618], asin(sqrt) = [619:868]
+# Node and network info = [1:25], CANOPUS = [25:27], SIRIUS/ZODIAC = [28:40], Library Hits = [41:67], analog hits = []
+# Blanks.RA = [112:119, 370], Area under the curve = [120:369], 
+# RA (blanks included) = [371:630], asin(sqrt) (blanks included) = [631:888]
 feature_table_combined <- 
   right_join(metadata,
-            left_join(
-              left_join(
-                feature_table_no_background, feature_relative_abundance,
-                by = "feature_number", suffix = c("" , ".RA")),
-              feature_asin_sqrt, by = "feature_number", suffix = c("", ".asin(sqrt)")), by = "feature_number")
+               left_join(
+                 left_join(
+                   feature_table_no_background, feature_relative_abundance,
+                   by = "feature_number", suffix = c("" , ".RA")),
+                 feature_asin_sqrt, by = "feature_number", suffix = c("", ".asin(sqrt)")),
+               by = "feature_number")
 
 feature_table_wdf <- feature_table_combined%>%
-  select(feature_number, everything())
+  dplyr::select(feature_number, everything())
 
-
+write_csv(feature_table_wdf, "moorea_feature_table_master_post_filtered.csv")
 # Making Mo’orea working data frame for stats -----------------------------
-
 moorea_transposed <- feature_table_wdf%>%
-  select(1, 619:868)%>%
-  gather(sample_ID, angular, 2:251)%>%
+  dplyr::select(feature_number, `Blank_Lot_6350565_01.asin(sqrt)`:ncol(.))%>%
+  gather(sample_ID, angular, 2:260)%>%
   spread(feature_number, angular)
+
 
 moorea_transposed$sample_ID <- moorea_transposed$sample_ID%>%
   gsub(".asin\\(sqrt)", "", .)%>%
@@ -203,7 +219,7 @@ mordor <- moorea_wdf%>%
 
 spiffy <- moorea_wdf%>%
   filter(Experiment == "SPIFFy")%>%
-  dplyr::select("Timepoint")
+  dplyr::dplyr::select("Timepoint")
 
 
 # Spiffy Workshopping -----------------------------------------------------
@@ -243,7 +259,7 @@ anova_sigs_spiffy <- anova_spiffy%>%
 spiffy_sig_names <-as.vector(anova_sigs_spiffy$feature_name)
 
 spiffy_only_sigs <- spiffy_no_bay%>%
-  select(c(1:4, spiffy_sig_names))%>%
+  dplyr::select(c(1:4, spiffy_sig_names))%>%
   group_by(reef_area)%>%
   summarize_if(is.numeric, mean)%>%
   ungroup()%>%
@@ -251,7 +267,7 @@ spiffy_only_sigs <- spiffy_no_bay%>%
   spread(reef_area, RA)
 
 spiffy_means_only <-spiffy_only_sigs%>%
-  select(-1)
+  dplyr::select(-1)
 
 spiffy_only_sigs$MaxNames <- colnames(spiffy_means_only)[max.col(spiffy_means_only, ties.method="first")]
 
@@ -273,7 +289,7 @@ spiffy_twolocals_network <- left_join(
 
 
 spiffy_rare <- spiffy_no_bay%>%
-  select(Site, reef_area, 5:8858)%>%
+  dplyr::select(Site, reef_area, 5:8858)%>%
   unite(site_name, c(Site, reef_area), sep = "_", remove = TRUE)%>%
   group_by(site_name)%>%
   summarize_if(is.numeric, mean)%>%
@@ -286,10 +302,10 @@ spiffy_rare$feature_name <- gsub(".RA", "", spiffy_rare$feature_name)
 
 spiffy_rare_byreeflocal <- spiffy_rare%>%
   ungroup()%>%
-  select(-max)%>%
+  dplyr::select(-max)%>%
   gather(site_name, RA, 2:16)%>%
   separate(site_name, c("site", "reef_area"), sep = "_")%>%
-  select(-site)%>%
+  dplyr::select(-site)%>%
   group_by(feature_name, reef_area)%>%
   summarize_if(is.numeric, mean)%>%
   ungroup()%>%
@@ -316,27 +332,28 @@ dorcierr_exudates_day <- dorcierr_wdf%>%
          !Replicate == 3,
          !Replicate == 4)
 
-## Need to take the mean of T0 to subtract from TF because of the differences in replicate #
+## Need to take the mean of T0 to subtract from TF because of the differences in replicate
+## This section needs to be redone because it is outdated
 dorcierr_transformations <- dorcierr_wdf%>%
-  select(c(1:16956))%>% 
+  dplyr::select(c(1:16956))%>% 
   gather(feature_name, RA, 5:16956)%>%
   spread(Timepoint, RA)
 
 dorcierr_t0 <- dorcierr_transformations%>%
-  select(-c(TF))%>%
+  dplyr::select(-c(TF))%>%
   filter(!Replicate == 3,
          !Replicate == 4)%>%
   group_by(Organism, DayNight, feature_name)%>%
   summarize_if(is.numeric, mean)
 
 dorcierr_newt0 <- left_join(dorcierr_transformations, dorcierr_t0, by = c("Organism", "DayNight", "feature_name"))%>%
-  select(-c(T0.x))%>%
+  dplyr::select(-c(T0.x))%>%
   rename(T0 = 'T0.y')
 
 dorcierr_newt0$change <- (dorcierr_newt0$TF - dorcierr_newt0$T0)
 
 dorcierr_transformed <- dorcierr_newt0%>%
-  select(-c(T0,TF))%>%
+  dplyr::select(-c(T0,TF))%>%
   spread(feature_name, change)
 
 dorcierr_day_transformed <- dorcierr_transformed%>%
@@ -344,6 +361,11 @@ dorcierr_day_transformed <- dorcierr_transformed%>%
 
 
 # Dorcierr Day Exudates (T0) ----------------------------------------------
+# average organism exudate dataframe
+average_RA <- dorcierr_exudates_day%>%
+  gather(feature_number, 6:ncol(.))
+
+# Oneway anova significant features
 p_values_oneway_day_exudates_dorc <- 
   as.data.frame(
     sapply(
@@ -352,38 +374,54 @@ p_values_oneway_day_exudates_dorc <-
 
 sig_one_way_day_exudates_dorc <- p_values_oneway_day_exudates_dorc%>%
   rownames_to_column(var = "feature_number")%>%
-  rename('p-value' = 2)
+  rename('p_value' = 2)%>%
+  filter(p_value, p_value < 0.05)
 
-dorcierr_exudates_day$Organism <- as.factor(dorcierr_exudates_day$Organism)
+sig_features_day_exudates_dorc <- as.factor(sig_one_way_day_exudates_dorc$feature_number)
 
-oneway_model_day_exudates_dorc <-
-  sapply(
-    dorcierr_exudates_day[6:ncol(dorcierr_exudates_day)],
-    function(x)  aov(x ~ dorcierr_exudates_day[["Organism"]]))
+# Dunnetts test testing ---------------------------------------------------
+## glht will run but gives confusing output. Trying to mull through it
+# Comes from Multcomp which gives problems with select function.
+set.seed(20140123)
+dorcierr_exudates_day_dunnetts <- dorcierr_exudates_day%>%
+  unite(org_rep, c("Organism", "Replicate"), sep = "_", remove = TRUE)%>%
+  gather(feature_name, asin, 5:13542)%>%
+  spread(org_rep, asin)%>%
+  add_column(sum = apply(.[5:16], 1, sum))%>%
+  filter(!sum == 0)%>%
+  dplyr::select(-sum)%>%
+  gather(org_rep, asin, 5:16)%>%
+  spread(feature_name, asin)%>%
+  separate(org_rep, c("Organism", "Replicate"), sep = "_", remove = TRUE)
 
-Dunnetts_day_exudates_dorc <-
-  sapply(
-    c(dorcierr_exudates_day[6:ncol(dorcierr_exudates_day)], dorcierr_exudates_day$Organism),
-    function(x, y)  
-      summary(
-        glht(
-          aov(x ~ dorcierr_exudates_day[["Organism"]]),
-          linfct = mcp(y = "Dunnett")))) 
+dunnett_model_exday <- lapply(dorcierr_exudates_day_dunnetts[6:11758], function(y, f)
+  summary(glht(aov(y ~ f, dorcierr_exudates_day_dunnetts), 
+               linfct = mcp(f = "Dunnett"))),
+  f = 
+    as.factor(
+      relevel(
+        factor(dorcierr_exudates_day$Organism), "Water control")))
 
-dunnett_test <- dorcierr_exudates_day%>%
-  gather(feature_number, asin, 6:16957)%>%
-  group_by(feature_number)%>% 
-    do(d_fit = glht(aov(asin ~ Organism, data = .),
-            linfct = mcp(Organism = "Dunnett")))
+p_values_dunnett_exudates_day <- as.data.frame(
+      sapply(
+        dunnett_model_exday,
+        function(x) x$test$pvalues))
+  
+  
+p_values_dunnett_exudates_day <- cbind("Organism" = c("CCA", "Dictyota", "Pocillopora verrucosa", "Porites Lobata", "Turf"), 
+                                       p_values_dunnett_exudates_day)
 
-dunnett_test <- dorcierr_exudates_day%>%
-  gather(feature_number, asin, 6:13443)%>%
-  group_by(feature_number)%>% 
-  do(d_fit = DunnettTest(.$asin, .$Organism, control = "Water control"))
+dunnett_FDR_exudates_day <- p_values_dunnett_exudates_day%>%
+  dplyr::select(c(sig_features_day_exudates_dorc))%>%
+  gather(feature_number, p_value, 2:ncol(.))
+
+dunnett_FDR_exudates_day$FDR_f <-p.adjust(dunnett_FDR_exudates_day$p_value, method = "BH")
+
+dunnett_sig_exudates_day <- dunnett_FDR_exudates_day%>%
+  filter(FDR_f, FDR_f < 0.05)%>%
+  dplyr::select(-p_value)%>%
+  spread(Organism, FDR_f)
 
 
-t2_fits <- grow_t2 %>% 
-  group_by(cSubjectID) %>% 
-  do(fitWeight_BR = lm(Weight_average_lin_interp  = Age + log(Age) + I(1/Age),
-                       data = grow_t2))
-
+  
+  
