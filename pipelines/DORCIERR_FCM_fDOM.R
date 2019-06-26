4d8db6f0a0443a85701c4a3a1a1a2adac2d1607a
Zquinlan <zquinlan@gmail.com>
2019-06-25 20:39:06 -0700

    Stats for linda and continuing Dorcierr analysis

--- pipelines/DORCIERR_FCM_fDOM.R
+++ pipelines/DORCIERR_FCM_fDOM.R
@@ -24,15 +24,28 @@ dorc_fcm_fdom <- read_xlsx("DORCIERR_fDOM_FCM.xlsx")%>%
   rename('DayNight' = 'Experiment')%>%
   rename(Organism = 'Organsim')%>%
   filter(DayNight == "Day")%>%
-  dplyr::select(-DayNight)
+  dplyr::select(-DayNight)%>%
+  mutate(Organism = case_when(Organism == "Water" ~ "Water control",
+                              TRUE ~ as.character(Organism)))
+
+dorc_metab <- read_csv("DORCIERR_metabolomics_wdf.csv")
+
+feature_metadata <- read_csv("moorea_feature_table_master_post_filtered.csv")%>%
+  dplyr::select(1:68)
 
 
-
+networking <- feature_metadata[c(1,17,23, 26, 33, 38, 29)]
+networking$feature_number <- as.character(networking$feature_number)
 
 # Subsetting to FCM or fDOM -----------------------------------------------
 fdom_wdf <- dorc_fcm_fdom%>%
-  dplyr::select(4:'PARAFAC6')%>%
-  filter(Timepoint == c('T0', 'TF'))
+  dplyr::select(c(4:'PARAFAC6'))%>%
+  filter(!Timepoint == "T1",
+         !Timepoint == "T2",
+         !Timepoint == "T3",
+         !Timepoint == "T4",
+         !Timepoint == "T5",
+         !Timepoint == "T6")
 
 fcm_wdf <- dorc_fcm_fdom%>%
   dplyr::select(c(1:7,33:ncol(.)))
@@ -79,12 +92,196 @@ fcm_rate_th_t0 <- left_join(fcm_th, fcm_t0, by = "Organism")%>%
   add_column(change_per_hour = (.$T4 - .$T0)/24)%>%
   add_column(log_change_per_hour = log10(.$change_per_hour))
 
-## ANOVA on the rate change and TF TEMPORARY. WILL RUN TOGETHER WITH FDOM DOC in TWO-WAY
-summary(aov(fcm_rate_t7_t0$log10_change_per_hour ~ fcm_rate_t7_t0$Organism))
-summary(aov(fcm_rate_t7_t0$log10_TF ~ fcm_rate_t7_t0$Organism))
 
-summary(aov(fcm_rate_th_t0$log_change_per_hour ~ fcm_rate_th_t0$Organism))
+# Metabolomics Cleaning and Subsetting ------------------------------------
+dorcierr_remins_day <- dorc_metab%>%
+  filter(DayNight == "Day")
 
+dorcierr_remins_cleaned <- dorcierr_remins_day%>%
+  dplyr::select(-c("Experiment", "DayNight"))%>%
+  unite(sample, c("Organism", "Timepoint", "Replicate" ), sep = "_", remove = TRUE)%>%
+  gather(feature_name, asin, 2:ncol(.))%>%
+  spread(sample, asin)%>%
+  add_column(sum = apply(.[2:37], 1, sum))%>%
+  filter(!sum == 0)%>%
+  dplyr::select(-sum)%>%
+  gather(sample, asin, 2:37)%>%
+  spread(feature_name, asin)%>%
+  separate(sample, c("Organism", "Timepoint", "Replicate"), sep = "_", remove = TRUE)
+
+dorcierr_exudates_day <- dorcierr_remins_cleaned%>%
+  filter(Timepoint == "T0",
+         !Replicate == 3,
+         !Replicate == 4)
+
+# fDOM Stats prep --------------------------------------------------------------
+fdom_log10 <-
+  as.data.frame(
+    sapply(
+      fdom_wdf[14:20],
+      function(x) log10(x)))%>%
+  add_column(sample_name = fdom_wdf$sample_name, .before = 1)%>%
+  add_column(Replicate = fdom_wdf$Replicate, .after = 1)%>%
+  add_column(Timepoint = fdom_wdf$Timepoint, .after = 1)%>%
+  add_column(Organism = fdom_wdf$Organism, .after = 1)
+
+
+## FCM stats for One-Way Anovas -----------------------------------------------------------
+#Combine dataframes into one
+fcm_stats_df <- left_join(fcm_t7, fcm_rate_th_t0[c(2:3,7)], by = c("Organism", "Replicate"))%>%
+  add_column(log10_TF = log10(.$TF), .before = 5)
+
+
+# Two-way Anova Stats prep ------------------------------------------------
+dom_stats_wdf <- full_join(fdom_log10, dorcierr_remins_cleaned, by = c("Organism", "Timepoint", "Replicate"))%>%
+  filter(!Organism == "Influent",
+         !Organism == "Offshore")
+
+
+
+# Running TWO-Way Anova DOM -------------------------------------------------
+# This line makes the names of the rows which will be added into the pvalue table
+twoway_anova_rows <- c("Organism", "Timepoint", "Organism*Timepoint")
+
+# The actual Model and collection of f_values
+aov_dom <- sapply(dom_stats_wdf[5:ncol(dom_stats_wdf)], 
+                  function(x) summary(
+                    aov(x ~ dom_stats_wdf[["Organism"]]*dom_stats_wdf[["Timepoint"]]))[[1]][1:3,'Pr(>F)'])
+
+
+two_way_dom <- as.data.frame(aov_dom)
+
+two_way_dom$anova_test <- cbind(twoway_anova_rows)
+
+two_way_tidy <- two_way_dom%>%
+  gather(feature, f_value, 1:12367)
+
+## Running Bejamini-Hochberg False Discovery Rate Corrections and filtering to significant values
+two_way_tidy$FDR <- p.adjust(two_way_tidy$f_value, method = "BH")
+
+two_way_signignificant <- two_way_tidy%>%
+  filter(FDR < 0.05)
+
+#Save siginificant values from test to a vector to filter out for post-hoc tests
+significant_organism_dom <- as.vector(two_way_signignificant%>% filter(anova_test == "Organism"))$feature
+significant_Timepoint_dom <- as.vector(two_way_signignificant%>% filter(anova_test == "Timepoint"))$feature
+
+
+## Writing the actual significance table to a folder
+write_csv(two_way_signignificant, "~/Documents/SDSU/DORCIERR/Datasets/stats/Dorcierr_two_way_significant.csv")
+
+
+# DOM Post-Hoc Tests ------------------------------------------------------
+## Making Post-Hoc dataframes filtering for significant p_values from Two-Way Anova
+dom_organism_post_hoc <- dom_stats_wdf%>%
+  dplyr::select(c(1:4, significant_organism_dom))
+
+dom_organism_exudates_ph <- dom_organism_post_hoc%>%
+  filter(Timepoint == "T0")%>%
+  dplyr::select(-sample_name)%>%
+  unite(sample, c("Organism", "Timepoint", "Replicate"), sep = "_", remove = TRUE)%>%
+  gather(feature_name, asin, 2:ncol(.))%>%
+  spread(sample, asin)%>%
+  add_column(sum = apply(.[2:ncol(.)], 1, sum))%>%
+  filter(!sum == 0)%>%
+  dplyr::select(-sum)%>%
+  gather(sample, asin, 2:ncol(.))%>%
+  spread(feature_name, asin)%>%
+  separate(sample, c("Organism", "Timepoint", "Replicate"), sep = "_", remove = TRUE)
+
+    
+dom_organism_remineralized_ph <- dom_organism_post_hoc%>%
+  filter(Timepoint == "TF")%>%
+  dplyr::select(-sample_name)%>%
+  unite(sample, c("Organism", "Timepoint", "Replicate"), sep = "_", remove = TRUE)%>%
+  gather(feature_name, asin, 2:ncol(.))%>%
+  spread(sample, asin)%>%
+  add_column(sum = apply(.[2:ncol(.)], 1, sum))%>%
+  filter(!sum == 0)%>%
+  dplyr::select(-sum)%>%
+  gather(sample, asin, 2:ncol(.))%>%
+  spread(feature_name, asin)%>%
+  separate(sample, c("Organism", "Timepoint", "Replicate"), sep = "_", remove = TRUE)
+
+
+dom_timepoint_post_hoc <- dom_stats_wdf%>%
+  dplyr::select(c(1:4, significant_organism_dom))
+
+
+## Running Dunnetts only both T0 and TF
+set.seed(2005)
+
+#Exudates T0
+dom_org_dunnetts_exudates <- lapply(dom_organism_exudates_ph[4:ncol(dom_organism_exudates_ph)], function(y, f)
+  summary(glht(aov(y ~ f, dom_organism_exudates_ph), 
+               linfct = mcp(f = "Dunnett"))),
+  f = 
+    as.factor(
+      relevel(
+        factor(dom_organism_exudates_ph$Organism), "Water control")))
+
+pvals_dunn_org_exudates_dom <- as.data.frame(
+  sapply(
+    dom_org_dunnetts_exudates,
+    function(x) x$test$pvalues))
+
+pvals_dunn_org_exudates_dom <- cbind("Organism" = c("CCA",
+                                                      "Dictyota", 
+                                                      "Pocillopora verrucosa",
+                                                      "Porites Lobata",
+                                                      "Turf"),
+                                     pvals_dunn_org_exudates_dom)
+
+dom_dunnetts_FDR_exudates <- pvals_dunn_org_exudates_dom%>%
+  gather(feature_number, p_value, 2:ncol(.))
+
+dom_dunnetts_FDR_exudates$FDR_f <-p.adjust(dom_dunnetts_FDR_exudates$p_value, method = "BH")
+
+dom_dunnett_sig_exudates <- dom_dunnetts_FDR_exudates%>%
+  filter(FDR_f, FDR_f < 0.05)%>%
+  dplyr::select(-p_value)%>%
+  spread(Organism, FDR_f)
+
+dunnett_sig_features_exudates <- as.vector(dom_dunnett_sig_exudates$feature_number)
+
+# Remineralized TF
+dom_org_dunnetts_remins <- lapply(dom_organism_remineralized_ph[4:ncol(dom_organism_remineralized_ph)], function(y, f)
+  summary(glht(aov(y ~ f, dom_organism_remineralized_ph), 
+               linfct = mcp(f = "Dunnett"))),
+  f = 
+    as.factor(
+      relevel(
+        factor(dom_organism_remineralized_ph$Organism), "Water control")))
+
+pvals_dunn_org_remins_dom <- as.data.frame(
+  sapply(
+    dom_org_dunnetts_remins,
+    function(x) x$test$pvalues))
+
+pvals_dunn_org_remins_dom <- cbind("Organism" = c("CCA",
+                                                    "Dictyota", 
+                                                    "Pocillopora verrucosa",
+                                                    "Porites Lobata",
+                                                    "Turf"),
+                                     pvals_dunn_org_remins_dom)
+
+dom_dunnetts_FDR_remins <- pvals_dunn_org_remins_dom%>%
+  gather(feature_number, p_value, 2:ncol(.))
+
+dom_dunnetts_FDR_remins$FDR_f <-p.adjust(dom_dunnetts_FDR_remins$p_value, method = "BH")
+
+dom_dunnett_sig_remins <- dom_dunnetts_FDR_remins%>%
+  filter(FDR_f, FDR_f < 0.05)%>%
+  dplyr::select(-p_value)%>%
+  spread(Organism, FDR_f)%>%
+
+
+dunnett_sig_features_remin <- as.vector(dom_dunnett_sig_remins$feature_number)
+
+dom_dunnett_remins_networking <- right_join(networking, dom_dunnett_sig_remins, by = "feature_number")
+
+
+# FCM Post-Hoc Tests ----------------------------------------------------------
 #Tukey on the rate change
 tukey_model_fcm <- 
   TukeyHSD(
@@ -121,35 +318,19 @@ fcm_graphing_wdf <- fcm_wdf%>%
          !Organism == "Offshore")%>%
   add_column(log10_cells = log10(.$`Cells µL-1`))%>%
   dplyr::mutate(Timepoint = case_when(Timepoint == "T0" ~ 0,
-                               Timepoint == "T1" ~ 4,
-                               Timepoint == "T2" ~ 16,
-                               Timepoint == "T3" ~ 19,
-                               Timepoint == "T4" ~ 24,
-                               Timepoint == "T5" ~ 30,
-                               Timepoint == "T6" ~ 39,
-                               Timepoint == "TF" ~ 47,
-                               TRUE ~ as.numeric(Timepoint)))
+                                      Timepoint == "T1" ~ 4,
+                                      Timepoint == "T2" ~ 16,
+                                      Timepoint == "T3" ~ 19,
+                                      Timepoint == "T4" ~ 24,
+                                      Timepoint == "T5" ~ 30,
+                                      Timepoint == "T6" ~ 39,
+                                      Timepoint == "TF" ~ 47,
+                                      TRUE ~ as.numeric(Timepoint)))
 
 ggplot(fcm_graphing_wdf, aes(x = Timepoint, y = log10_cells, col = Organism))+
   geom_point(stat = "summary", fun.y = "mean")+
   stat_summary(fun.y=mean, geom="line")
 
-
-# fDOM Stats prep --------------------------------------------------------------
-fdom_log10 <-
-  as.data.frame(
-    sapply(
-      fdom_wdf[14:20],
-      function(x) log10(x)))%>%
-  add_column(sample_name = fdom_wdf$sample_name, .before = 1)%>%
-  add_column(Replicate = fdom_wdf$Replicate, .after = 1)%>%
-  add_column(Timepoint = fdom_wdf$Timepoint, .after = 1)%>%
-  add_column(Organism = fdom_wdf$Organism, .after = 1)
-
-
-# Overall Stats -----------------------------------------------------------
-#Combine dataframes into one
-stats_df <- left_join
 
 
 
--- pipelines/RR3_stats_062019.R
+++ pipelines/RR3_stats_062019.R
@@ -4,7 +4,9 @@ rr3_features <- read_tsv("AnnotatedRR3FeaturesForZach.txt")
 
 moorea_binned <- moorea_post_stats[c(1, 948:963)]
   
+rr3_features_check <- as.vector(rr3_features$feature_number)
 
+moorea_binned_check <- inner_join(moorea_binned, rr3_features, by = "feature_number")
 
 rr3_dunnetts <- left_join(rr3_features, moorea_binned, by = "feature_number")
 
@@ -44,17 +46,17 @@ water_day_v_night <-
       water_dvn[4:ncol(water_dvn)],
       function(x) summary(aov(x ~ water_dvn[["DayNight"]]))[[1]][1,'Pr(>F)']))%>%
   rownames_to_column(var = "feature_number")%>%
-  rename(p_value_water = 2)
+  rename(p_value = 2)
+
+water_day_v_night$FDR_water <- p.adjust(water_day_v_night$p_value, method = "BH")
 
 
-porites_day_v_night <- 
-  as.data.frame(
-    sapply(
-      porites_dvn[4:ncol(porites_dvn)],
+porites_day_v_night <- as.data.frame(sapply(porites_dvn[4:ncol(porites_dvn)],
       function(x) summary(aov(x ~ porites_dvn[["DayNight"]]))[[1]][1,'Pr(>F)']))%>%
   rownames_to_column(var = "feature_number")%>%
-  rename(p_value_porities = 2)%>%
-  
+  rename(p_value = 2)
+
+porites_day_v_night$FDR_porites <- p.adjust(porites_day_v_night$p_value, method = "BH")
 
 pocil_day_v_night <- 
   as.data.frame(
@@ -62,7 +64,9 @@ pocil_day_v_night <-
       pocil_dvn[4:ncol(pocil_dvn)],
       function(x) summary(aov(x ~ pocil_dvn[["DayNight"]]))[[1]][1,'Pr(>F)']))%>%
   rownames_to_column(var = "feature_number")%>%
-  rename(p_value_pocillopora = 2)
+  rename(p_value = 2)
+
+pocil_day_v_night$FDR_pocillopora <- p.adjust(pocil_day_v_night$p_value, method = "BH")
 
 cca_day_v_night <- 
   as.data.frame(
@@ -70,7 +74,9 @@ cca_day_v_night <-
       cca_dvn[4:ncol(cca_dvn)],
       function(x) summary(aov(x ~ cca_dvn[["DayNight"]]))[[1]][1,'Pr(>F)']))%>%
   rownames_to_column(var = "feature_number")%>%
-  rename(p_value_cca = 2)
+  rename(p_value = 2)
+
+cca_day_v_night$FDR_cca <- p.adjust(cca_day_v_night$p_value, method = "BH")
 
 turf_day_v_night <- 
   as.data.frame(
@@ -78,7 +84,9 @@ turf_day_v_night <-
       turf_dvn[4:ncol(turf_dvn)],
       function(x) summary(aov(x ~ turf_dvn[["DayNight"]]))[[1]][1,'Pr(>F)']))%>%
   rownames_to_column(var = "feature_number")%>%
-  rename(p_value_turf = 2)
+  rename(p_value = 2)
+
+turf_day_v_night$FDR_turf <- p.adjust(turf_day_v_night$p_value, method = "BH")
 
 dictyota_day_v_night <- 
   as.data.frame(
@@ -87,6 +95,8 @@ dictyota_day_v_night <-
       function(x) summary(aov(x ~ dictyota_dvn[["DayNight"]]))[[1]][1,'Pr(>F)']))%>%
   rownames_to_column(var = "feature_number")%>%
   rename(p_value_dictyota = 2)
+
+dictyota_day_v_night$FDR_dictyota <- p.adjust(dictyota_day_v_night$p_value, method = "BH")
 
 
 p_values_combined <- 
@@ -100,5 +110,129 @@ p_values_combined <-
       ), turf_day_v_night, by = "feature_number"
     ), dictyota_day_v_night, by = "feature_number"
   )
+
+
+# 06/25/2019 --------------------------------------------------------------
+
+rr3_new <- read_xlsx("MappedRRMetabolitesForCraig.xlsx")
+
+feature_asin_sqrt <-
+  as.data.frame(
+    sapply(
+      rr3_new[5:ncol(rr3_new)],
+      function(x) asin(sqrt(x))))%>%
+  add_column(level_4 = rr3_new$`Level 4`, .before = 1)
+
+rr3_aov <- feature_asin_sqrt%>%
+  gather(Organism, asin, 2:ncol(.))%>%
+  spread(level_4, asin)
+
+rr3_aov$Organism <- rr3_aov$Organism%>% 
+  gsub("__1", "", .)%>%
+  gsub("__2", "", .)%>%
+  gsub("__3", "", .)%>%
+  gsub("2", "", .)%>%
+  gsub("3", "", .)
+
+
+rr3_ra <- rr3_new%>%
+  dplyr::select(-c(1:3))%>%
+  gather(Organism, asin, 2:ncol(.))%>%
+  spread(`Level 4`, asin)
+
+rr3_ra$Organism <- rr3_ra$Organism%>% 
+  gsub("__1", "", .)%>%
+  gsub("__2", "", .)%>%
+  gsub("__3", "", .)%>%
+  gsub("2", "", .)%>%
+  gsub("3", "", .)
+
+#Anova
+aov_rr3_model <- as.data.frame(
+  sapply(
+    rr3_aov[2:ncol(rr3_aov)],
+    function(x) summary(aov(x ~ rr3_aov[["Organism"]]))[[1]][1,'Pr(>F)']))
+
+aov_rr3_df <- aov_rr3_model%>%
+  rownames_to_column(var = "feature_number")%>%
+  rename("p_value" = 2)
+
+
+
+## Running Bejamini-Hochberg False Discovery Rate Corrections and filtering to significant values
+aov_rr3_df$FDR <- p.adjust(aov_rr3_df$p_value, method = "BH")
+
+one_way_signignificant <- aov_rr3_df%>%
+  filter(FDR < 0.05)
+
+#Save siginificant values from test to a vector to filter out for post-hoc tests
+significant_aov_rr3 <- as.vector(one_way_signignificant$feature)
+
+
+## Dunnetts
+rr3_dunnetts <- lapply(rr3_aov[2:ncol(rr3_aov)], function(y, f)
+  summary(glht(aov(y ~ f, rr3_aov), 
+               linfct = mcp(f = "Dunnett"))),
+  f = 
+    as.factor(
+      relevel(
+        factor(rr3_aov$Organism), "WAT")))
+
+pvals_dunn_rr3 <- as.data.frame(
+  sapply(
+    rr3_dunnetts,
+    function(x) x$test$pvalues))
+
+pvals_dunn_rr3 <- cbind("Organism" = c("CCA",
+                                       "Dictyota", 
+                                       "Pocillopora verrucosa",
+                                       "Porites Lobata",
+                                       "Turf"),
+                        pvals_dunn_rr3)
+
+rr3_dunnetts_FDR <- pvals_dunn_rr3%>%
+  gather(feature_number, p_value, 2:ncol(.))
+
+rr3_dunnetts_FDR$FDR_f <-p.adjust(rr3_dunnetts_FDR$p_value, method = "BH")
+
+rr3_dunnett_sigs <- rr3_dunnetts_FDR%>%
+  filter(FDR_f, FDR_f < 0.05)%>%
+  dplyr::select(-p_value)%>%
+  spread(Organism, FDR_f)
+
+## Tukeys
+tukey_rr3_model <- sapply(rr3_aov[2:ncol(rr3_aov)], function(x)
+  TukeyHSD(aov(x ~ rr3_aov$Organism, data = rr3_aov), p.adjust.methods = "BH"))
+
+p_values_tukey_rr3 <- as.data.frame(tukey_rr3_model)%>%
+  rownames_to_column(var = "variable")%>%
+  gather(feature_info, value, 2:ncol(.))%>%
+  filter(feature_info %like% '%p.adj%')%>%
+  filter(value, value < 0.05)
+
+p_values_tukey_rr3$feature_info <- p_values_tukey_rr3$feature_info%>%
+  gsub(".rr3_aov.Organism.p.adj","", .)
+
+# Tukeys for diff columns
+tukey_rr3_ra_model <- sapply(rr3_ra[2:ncol(rr3_ra)], function(x)
+  TukeyHSD(aov(x ~ rr3_ra$Organism, data = rr3_ra), p.adjust.methods = "BH"))
+
+diff_tukey_rr3 <- as.data.frame(tukey_rr3_ra_model)%>%
+  rownames_to_column(var = "variable")%>%
+  gather(feature_info, value, 2:ncol(.))%>%
+  filter(feature_info %like% '%diff%')
+
+diff_tukey_rr3$feature_info <- diff_tukey_rr3$feature_info%>%
+  gsub(".rr3_ra.Organism.diff","", .)
+
+p_values_diffs_tukey_rr3 <- left_join(p_values_tukey_rr3, diff_tukey_rr3, by = c("variable", "feature_info" ))%>%
+  rename(p_value = 3, 
+         diff = 4)
+
+## writing the sig tables
+write_csv(p_values_diffs_tukey_rr3, "rr3_mapped_tukey_significant.csv")
+write_csv(rr3_dunnett_sigs, "rr3_mapped_dunnetts_significant.csv")
+
+
 
 
